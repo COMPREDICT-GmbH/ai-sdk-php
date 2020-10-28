ded89e995415e3f1e60d4dd09fb56b3176d69ee8
Muhamet Sherifi <sherifimuhamet@gmail.com>
2020-10-28 11:50:54 +0100

    wip

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Compredict\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function it_will_return_true()
+    {
+        $this->assertTrue(true);
+    }
+}

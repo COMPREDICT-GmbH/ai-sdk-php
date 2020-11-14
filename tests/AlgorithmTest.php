581a14c26418ccab2d713e9fb42b0baf027fbf01
Muhamet Sherifi <sherifimuhamet@gmail.com>
2020-11-14 02:17:16 +0100

    wip

---
+++ tests/AlgorithmTest.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Compredict\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AlgorithmTest extends TestCase
+{
+}
---
+++ tests/MockClient.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Compredict\Tests;
+
+
+class MockClient
+{
+
+}
